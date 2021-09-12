e6c408858340d8b5d99cfff5f4b6691ae64dee18
Alwin John <alwinjohn231@gmail.com>
2021-09-12 12:04:02 +0530

    TopHeader component and styles added

--- src/components/TopHeader/TopHeader.js
+++ src/components/TopHeader/TopHeader.js
@@ -1,11 +1,18 @@
 /* eslint-disable @next/next/link-passhref */
 import Link from 'next/link';
 import React from 'react';
-import { Container,NavLink  } from './TopHeaderStyles';
+import { Container,NavLink, Div1, Span } from './TopHeaderStyles';
 const TopHeader = () => (
     <Container>
-    <Link href="/"><NavLink>Home</NavLink></Link>
-    <Link href="/about"><NavLink>About Us</NavLink></Link>
+        <Div1>
+    <Link href="https://www.ieee.org/" passHref><NavLink>IEEE.org</NavLink></Link><Span>|</Span>
+    <Link href="https://ieeexplore.ieee.org/Xplore/home.jsp" passHref><NavLink>IEEE Xplore Digital Library</NavLink></Link><Span>|</Span>
+    <Link href="https://standards.ieee.org/" passHref><NavLink>IEEE Standards</NavLink></Link><Span>|</Span>
+    <Link href="https://spectrum.ieee.org/" passHref><NavLink>IEEE Spectrum</NavLink></Link><Span>|</Span>
+    <Link href="https://ieeekerala.org/" passHref><NavLink>IEEE Kerala Section</NavLink></Link><Span>|</Span>
+    <Link href="https://www.ieeer10.org/" passHref><NavLink>IEEE Region 10</NavLink></Link><Span>|</Span>
+    <Link href="https://www.ieee.org/sitemap.html" passHref><NavLink>More Sites</NavLink></Link>
+    </Div1>
     </Container>
 );
 
--- src/components/TopHeader/TopHeaderStyles.js
+++ src/components/TopHeader/TopHeaderStyles.js
@@ -4,14 +4,22 @@ export const Container = styled.div`
   background-color: ${props => props.theme.colors.primary1};
   display: flex;
   flex-direction: row;
+`;
+
+
+export const Div1 = styled.div`
+  padding:0.4rem 1.8rem;
   @media ${(props) => props.theme.breakpoints.sm} {
     justify-content: center;
     align-items: center;
+    padding:0.4rem 2.2rem;
   }
 `;
 
 export const NavLink = styled.a`
   font-size: 14px;
+  padding-left:1.2rem;
+  padding-right:1.2rem;
   line-height: 32px;
   color: white;
   transition: 0.4s ease;
@@ -22,5 +30,13 @@ export const NavLink = styled.a`
   }
   @media ${(props) => props.theme.breakpoints.sm} {
     padding: 0.5rem;
+    font-size:12px;
+    line-height: 26px;
+    
   }
+`;
+
+export const Span = styled.span`
+  color: white;
+  font-size: 14px;
 `;
