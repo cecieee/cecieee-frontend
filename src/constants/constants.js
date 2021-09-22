5a11d9588847261592776bdd0a7af57c0153c3ad
Alwin <alwinjohn231@gmail.com>
2021-09-22 12:07:01 +0530

    constants folder added for to seperate data and UI

---
+++ src/constants/constants.js
@@ -0,0 +1,11 @@
+export const testimonials = [
+    {
+     testimonial:'Our journey has only brought us higher. IEEE computer society gave and continues to give me great opportunities for personal knowledge and career improvement.',
+      
+    },
+    {
+    
+        testimonial:"Our journey has only brought us higher. IEEE computer society gave and continues to give me great opportunities for personal knowledge and career improvement.",
+        
+      }
+  ];
