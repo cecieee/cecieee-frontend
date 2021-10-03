06f3b602390fce75f204421de7113d5516bcdee9
Alwin <alwinjohn231@gmail.com>
2021-10-03 17:28:09 +0530

    components created for home page by Danish

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import { Container, Main, Heading1} from '../styles/HomeStyles';
 import TopHeader from '../components/TopHeader/TopHeader';
 import Header from '../components/Header/Header';
+import VisionMision from './../components/VisionMision';
+import About from '../components/About';
+import { Slider } from '../components/Slider';
 import Testimonials from '../components/Testimonials/Testimonials';
 import Footer from './../components/Footer/Footer';
 
@@ -10,12 +13,14 @@ export default function Home() {
       <Main>
         <TopHeader/>
         <Header/>
+        <Slider />
+        <VisionMision/>
+          <About/>
         <Heading1>
             Testimonials
           </Heading1>
           <Testimonials/>
           <Footer/>
-          
         </Main>
     </Container>
   )
