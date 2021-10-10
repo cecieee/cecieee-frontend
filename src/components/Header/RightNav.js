04bc04dd9e108afc147e8fc022a3d2cbf067426f
Alwin <alwinjohn231@gmail.com>
2021-10-10 15:45:19 +0530

    added diffrent font size and styles in mobile view

--- src/components/Header/RightNav.js
+++ src/components/Header/RightNav.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ActiveLink from '../ActiveLink';
 import Link from 'next/link';
+import AboutElement from './AboutElements';
+import ChapterElement from './ChapterElements';
 import { NavLink,Div2} from './HeaderStyles';
 const RightNav = ({ open }) => {
   return (
 
     <Div2 open={open}>
     <ActiveLink href="/"><NavLink>Home</NavLink></ActiveLink>
-    <ActiveLink href="/about"><NavLink>About Us</NavLink></ActiveLink>
-    <ActiveLink href=""><NavLink>Chapters</NavLink></ActiveLink>
+    <ActiveLink href="/about"><NavLink>About Us<AboutElement/></NavLink></ActiveLink>
+    
+    <ActiveLink href=""><NavLink>Chapters<ChapterElement/></NavLink></ActiveLink>
     <ActiveLink href="/event-list"><NavLink>Events</NavLink></ActiveLink>
     <ActiveLink href="/news"><NavLink>News</NavLink></ActiveLink>
     <ActiveLink href="/constitution"><NavLink>Constitution</NavLink></ActiveLink>
