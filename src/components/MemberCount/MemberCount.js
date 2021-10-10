f6722eae72ef5584c681b6e84904d733789ba1a6
noel-jose <noeljosetp@gmail.com>
2021-10-10 12:28:17 +0530

    Created MemberCount component and added image

---
+++ src/components/MemberCount/MemberCount.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function MemberCount() {
+    return (
+        <Section>
+            
+        </Section>
+    )
+}
+
+export default MemberCount
+
+const Section = styled.section`
+    height:450px;
+    background-size:cover;
+    background-image: url("./images/team.png");
+    background-attachment: fixed;
+`;
