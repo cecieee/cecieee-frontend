3c87fa68e624385408b4dabb950308ea9a6ca86e
noel-jose <noeljosetp@gmail.com>
2021-11-12 22:54:08 +0530

    Added more articles to the Constitution page

--- src/components/Constitution/Laws.js
+++ src/components/Constitution/Laws.js
@@ -44,18 +44,186 @@ function Laws() {
                     <li><Sub>C.</Sub>  For impeachment, a notice must be sent to the respective ExCom member and a fair chance must be given to explain themselves before ExCom and if still 2/3rd of the members of the ExCom support impeachment, he/she shall be removed from the post.</li>
                     <li><Sub>D.</Sub>  For impeachment, Counselor and at least two SBC/AG/Society advisors must be present in the meeting.</li>
                     <li><Sub>E.</Sub>  Respective Society/AG advisor must be present in the ExCom meeting to impeach the Society/AG ExCom member.</li>
-                    <li><Sub>F.</Sub>  SB Treasurer</li>
-                    <li><Sub>G.</Sub>  All Society Advisors, Chairs, Vice-Chairs and Secretaries</li>
-                    <li><Sub>H.</Sub>  All Affinity Group Advisors, Chairs, Vice-Chairs and Secretaries</li>
-                    <li><Sub>I.</Sub>  Coordinators of all Special Interest Groups</li>
-                    <li><Sub>J.</Sub>  Webmaster</li>
-                    <li><Sub>K.</Sub>  Electronic Communication Coordinator</li>
-                    <li><Sub>L.</Sub>  Membership Development Coordinator</li>
-                    <li><Sub>M.</Sub>  LINK Representative</li>
-                    <li><Sub>N.</Sub>  Student Branch Mentor</li>
                 </SubList>
                 </li>
-           
+                <Article>Article III: Roles and Responsibilities</Article>
+                <List>
+                    <li><Sub>Sec. 1. SB Chairperson: <br /><br /></Sub>
+                    <SubList>
+                        <li><Sub>A.</Sub> Responsible for the overall management of all Branch affairs.</li>
+                        <li><Sub>B.</Sub>  Preside all meetings of the Student Branch.</li>
+                        <li><Sub>C.</Sub>  Hold regular meetings of the Branch Executive Committee.</li>
+                        <li><Sub>D.</Sub>  Appoint Program, Publicity and Membership committee Chairs promptly.</li>
+                        <li><Sub>E.</Sub>  Complete the annual plan in vTools – due no later than January 31st.</li>
+                        <li><Sub>F.</Sub>  Arrange for the selection of new Officers annually and report via vTools Officer Reporting.</li>
+                        <li><Sub>G.</Sub>  Ensure the smooth transition of information and materials to newly elected Officers and arrange an orderly transfer of Student Branch records.</li>
+                        <li><Sub>H.</Sub>  Coordinate activities with local Section and Regional volunteers.</li>
+                    </SubList>
+                    </li>
+                    <li><Sub>Sec. 2. SB Vice-Chair: <br /><br /></Sub>
+                    <SubList>
+                        <li><Sub>A.</Sub> Assist the Student Branch Chair in all Student Branch affairs.</li>
+                        <li><Sub>B.</Sub>  Perform all functions of the Branch Chair in his/her absence or upon request.</li>
+                        <li><Sub>C.</Sub>  Use OU Analytics and provide the required data to the Membership Development Coordinator.</li>
+                        <li><Sub>D.</Sub>  Download the membership data from OU analytics (overall, Chapters & AG) on the first day of every month and handover to Membership Development Coordinator.</li>
+                    </SubList>
+                    </li>
+                    <li><Sub>Sec. 3. SB Secretary: <br /><br /></Sub>
+                    <SubList>
+                        <li><Sub>A.</Sub> Prepare the Agenda of General Body Meetings/ExCom Meetings and send to all SB Members/ExCom Members.</li>
+                        <li><Sub>B.</Sub>  The Student Branch Secretary serves as the record keeper and historian of the Student Branch. He/she is responsible for maintaining all records.</li>
+                        <li><Sub>C.</Sub>  Keep detailed minutes of each meeting and circulate the deets of the meeting to all ExCom members.</li>
+                        <li><Sub>D.</Sub>  Be responsible for all correspondence and the same must be mentioned in the correspondence register.</li>
+                        <li><Sub>E.</Sub>  Ensure that the Branch Constitution and Bylaws are adhered to.</li>
+                        <li><Sub>F.</Sub>  Arrange for an orderly transfer of all records to the incoming Secretary.</li>
+                        <li><Sub>G.</Sub>  Must follow the IEEE SB Development Calendar.</li>
+                    </SubList>
+                    </li>
+                    <li><Sub>Sec. 4. SB Joint-Secretary: <br /><br /></Sub>
+                    <SubList>
+                        <li><Sub>A.</Sub> Perform all functions of the Student Branch Secretary in his/her absence or upon request.</li>
+                        <li><Sub>B.</Sub>  Ensure all activity reports of SB, SBC, AG and SIG are completed and reported timely by respective Secretaries.</li>
+                        <li><Sub>C.</Sub>   Keep track of the activity calendar and no collision occur in those activities.</li>
+                        <li><Sub>D.</Sub>  Transfers records to the new Joint-Secretary at the end of the year.</li>
+                    </SubList>
+                    </li>
+                    <li><Sub>Sec. 5. SB Treasurer: <br /><br /></Sub>
+                    <SubList>
+                        <li><Sub>A.</Sub> Maintain the appropriate financial accounts. SB Bank account should require two signatures; the Branch Counselor and the SB Treasurer.</li>
+                        <li><Sub>B.</Sub>  Prepare an annual financial report for inclusion in the Annual Activity Report (due 1 November annually).</li>
+                        <li><Sub>C.</Sub>  Prepare the final Financial Statement for inclusion in the Annual Activity Report.</li>
+                        <li><Sub>D.</Sub>  Oversee all fundraising efforts.</li>
+                        <li><Sub>E.</Sub>  Arrange for an orderly transfer of all financial records to the incoming Treasurer.</li>
+                        <li><Sub>F.</Sub>  SB Treasurer must produce a request letter stating the reason for withdrawal to SB Counselor to withdraw the amount from SB Account and the same request must be maintained in the finance register/file.</li>
+                        <li><Sub>G.</Sub>  Serve as the treasurer for all events happening in the SB.</li>
+                    </SubList>
+                    </li>
+                    <li><Sub>Sec. 6. SB Counselor: <br /><br /></Sub>
+                    <SubList>
+                        <li><Sub>A.</Sub> Serve as an advisor to the Student Branch.</li>
+                        <li><Sub>B.</Sub>  Ensure that information from IEEE Headquarters is transmitted to the student Officers.</li>
+                        <li><Sub>C.</Sub>  Attend Executive Committee meetings and assist the committees.</li>
+                        <li><Sub>D.</Sub>  Participate in Regional Activities Committee meetings.</li>
+                        <li><Sub>E.</Sub>  Consult with the Section SAC Chair, Regional SAC Chair, or Region Director about activities or problems.</li>
+                        <li><Sub>F.</Sub>  Foster good relations with the local Section and encourage students to establish regular liaison with the Section SAC Chair.</li>
+                        <li><Sub>G.</Sub>  Promote student awareness of awards, contests and benefits of IEEE Membership.</li>
+                        <li><Sub>H.</Sub>  Establish industrial contacts for programs and activities.</li>
+                        <li><Sub>I.</Sub>  Assist with promoting activities to other faculty members.</li>
+                    </SubList>
+                    </li>
+                    <li><Sub>Sec. 7. Chapter/Affinity Group Chair: <br /><br /></Sub>
+                    <SubList>
+                        <li><Sub>A.</Sub> Preside at meetings of the Chapter and the Chapter Executive Committee.</li>
+                        <li><Sub>B.</Sub>  Operate the Chapter to maximize the satisfaction of Chapter member needs with the resources available.</li>
+                        <li><Sub>C.</Sub>  Develop and present plans for meeting members’ needs to the Chapter Executive Committee for review.</li>
+                        <li><Sub>D.</Sub>  Identify appropriate volunteers and present their appointments to the Chapter Executive Committee for review.</li>
+                        <li><Sub>E.</Sub>  Ensure that all Chapter Officers and Committee Chairs are properly trained for their jobs.</li>
+                        <li><Sub>F.</Sub>  Sign off on the financial report where required and confirm that all reporting is accurate and submitted in a timely manner.</li>
+                        <li><Sub>G.</Sub>  Ensure satisfactory performance from the other Chapter Officers and the Chapter Committee Chairs.</li>
+                        <li><Sub>H.</Sub>  Prepare an annual report of Chapter Activities for submission to the Section Secretary, Region and/or Society Chapter Coordinators.</li>
+                        <li><Sub>I.</Sub>  Represent the Chapter at all IEEE gatherings.</li>
+                        <li><Sub>J.</Sub>  Transfer Chapter records to the new Chapter Chair at the end of the year.</li>
+                    </SubList>
+                    </li>
+                    <li><Sub>Sec. 8. Chapter/Affinity Group Vice-Chair: <br /><br /></Sub>
+                    <SubList>
+                        <li><Sub>A.</Sub> Assist the SBC/AG Chair in all SBC/AG affairs.</li>
+                        <li><Sub>B.</Sub>  Perform all functions of the SBC/AG Chair in his/her absence or upon request.</li>
+                    </SubList>
+                    </li>
+                    <li><Sub>Sec. 9. Chapter/Affinity Group Secretary: <br /><br /></Sub>
+                    <SubList>
+                        <li><Sub>A.</Sub> Serve as the record keeper and historian of the Chapter/Affinity Group. He/she is responsible for maintaining all records of the Chapter/Affinity Group.</li>
+                        <li><Sub>B.</Sub>  Keep detailed minutes of each meeting and submit a detailed report of all events/meetings to the SB Secretary.</li>
+                        <li><Sub>C.</Sub>  Keep track of all correspondences related to the Chapter/Affinity Group and work along with SB Secretary regarding the same.</li>
+                        <li><Sub>D.</Sub>  Work along with SB Treasurer regarding all financial matters.</li>
+                        <li><Sub>E.</Sub>  Arrange for an orderly transfer of all records to the incoming Secretary</li>
+                        <li><Sub>F.</Sub>  Must follow the IEEE SB Development Calendar.</li>
+                    </SubList>
+                    </li>
+                    <li><Sub>Sec. 10. Chapter/Affinity Group Advisor: <br /><br /></Sub>
+                    <SubList>
+                        <li><Sub>A.</Sub> Serves as an Advisor to the Chapter/Affinity Group.</li>
+                        <li><Sub>B.</Sub>  SB Treasurer must coordinate and seek advice from the Chapter/Affinity Group Advisor regarding the financial matters of the Chapter.</li>
+                    </SubList>
+                    </li>
+                    <li><Sub>Sec. 11. Membership Development Coordinator: <br /><br /></Sub>
+                    <SubList>
+                        <li><Sub>A.</Sub> Responsible to conduct Membership Development Sessions for first-year students and non-members in other years.</li>
+                        <li><Sub>B.</Sub>  Ensure that the recruitment and renewal process is done timely.</li>
+                        <li><Sub>C.</Sub>   Must make use of group challan for all IEEE membership payments for more than 5 persons.</li>
+                        <li><Sub>D.</Sub>  Must ensure that all ExCom members have IEEE Memberships.</li>
+                        <li><Sub>E.</Sub>   Get the SB membership data from the Vice-Chair and analyze it to understand the trends and take steps accordingly.</li>
+                        <li><Sub>F.</Sub>  Get the membership count and data from the Vice-Chair every month and maintain a record.</li>
+                        <li><Sub>G.</Sub>  Ensure that the data is used with utmost care and it is not shared with anyone else.</li>
+                    </SubList>
+                    </li>
+                    <li><Sub>Sec. 12. Electronic Communication Coordinator: <br /><br /></Sub>
+                    <SubList>
+                        <li><Sub>A.</Sub> Must update the IEEE notice board periodically.</li>
+                        <li><Sub>B.</Sub>  Manage the social media channels of the Student Branch and ensure that the best practices are followed while updating the channels.</li>
+                        <li><Sub>C.</Sub>  Manage the design team and ensure all the designs follow the IEEE Visual Identity Guidelines.</li>
+                        <li><Sub>D.</Sub>  Ensure that the members are informed about all updates (events, announcements) happening in the Student Branch.</li>
+                    </SubList>
+                    </li>
+                    <li><Sub>Sec. 13. Webmaster: <br /><br /></Sub>
+                    <SubList>
+                        <li><Sub>A.</Sub> To maintain the Student Branch Website.</li>
+                        <li><Sub>B.</Sub>  To ensure that all IEEE Guidelines regarding website are adhered to.</li>
+                        <li><Sub>C.</Sub>  Work hand in hand with SB, SBC and AG Secretaries and Electronic Communication Coordinator to ensure that details on the website are updated regularly.</li>
+                    </SubList>
+                    </li>
+                    <li><Sub>Sec. 14. LINK Representative: <br /><br /></Sub>
+                    <SubList>
+                        <li><Sub>A.</Sub> A. LINK Representative shall act as a link between the Student Branch and the LINK.</li>
+                    </SubList>
+                    </li>
+                    <li><Sub>Sec. 15. Coordinators of all Special Interest Groups: <br /><br /></Sub>
+                    <SubList>
+                        <li><Sub>A.</Sub> Coordinate the activities of the particular Special Interest Group within the Student Branch.</li>
+                        <li><Sub>B.</Sub>  Represent the Special Interest Group in IEEE gatherings.</li>
+                    </SubList>
+                    </li>
+                    <li><Sub>Sec. 16. Student Branch Mentor: <br /><br /></Sub>
+                    <SubList>
+                        <li><Sub>A.</Sub> Student Branch Mentor shall be an Alumni of the Student Branch who has graduated at least three years before, who is still an IEEE member and maintains a good relation with IEEE Kerala Section.</li>
+                        <li><Sub>B.</Sub>  Selection of the mentor is the responsibility of the SB ExCom.</li>
+                        <li><Sub>C.</Sub>  The mentor shall be an invitee to the SB ExCom and does not have any voting rights.</li>
+                        <li><Sub>D.</Sub>  SB can approach the mentor for all sorts of advice and guidance.</li>
+                    </SubList>
+                    </li>
+                </List>
+            </List>
+            <Article>Article IV: Quorum</Article>
+            <List>
+                <li><Sub>Sec. 1.</Sub> Quorum, by default, unless otherwise specified will be half the number of voting members.</li>
+                <li><Sub>Sec. 2.</Sub> Quorum is not applicable for convening AGM and GMs.</li>
+                <li><Sub>Sec. 3.</Sub> The presiding officer, if he/she feels necessary, may insist 2/3rd voting quorum be present to decide on a particular agenda of importance.</li>
+                <li><Sub>Sec. 4.</Sub> The presiding officer of meetings will not have voting rights unless his/her vote changes the odds of the decision or unless voting is by secret ballot.</li>
+            </List>
+            <Article>Article V: General Body Meetings</Article>
+            <List>
+                <li><Sub>Sec. 1.</Sub> The Student Branch shall hold regular meetings at places and time as decided by the Executive Committee. At least 4 General Body meetings must be conducted in a year.</li>
+                <li><Sub>Sec. 2.</Sub> SB Annual General Meeting and General Body Meetings can only be convened with one-week prior notice to the members.</li>
+                <li><Sub>Sec. 3.</Sub> The Student Branch General Body consists of all the student members of SB.</li>
+                <li><Sub>Sec. 4.</Sub>  The General Body is the supreme authority and its decisions shall be final and binding.</li>
+                <li><Sub>Sec. 5.</Sub>  Agenda of the meeting must be conveyed to members prior to the meeting.</li>
+                <li><Sub>Sec. 6.</Sub>  Any member can ask the SB Secretary in writing to include in agenda any specific matter requiring discussion or attention of the General Body.</li>
+                <li><Sub>Sec. 7.</Sub>  Discussion on any topic not specified in the agenda of the meeting will be at the discretion of the presiding officer.</li>
+            </List>
+            <Article>Article VI: SB Chapters and Affinity Groups</Article>
+            <List>
+                <li><Sub>Sec. 1.</Sub> SB Chapters and Affinity Groups are not independent OUs but are under the governance of the Student Branch.</li>
+                <li><Sub>Sec. 2.</Sub> The Student Branch Chapter or Affinity Group General Body consists of all the student members of the particular Society.</li>
+                <li><Sub>Sec. 3.</Sub> The Student Branch Chapter or Affinity Group must periodically conduct SBC or AG General Body Meetings at least once in a month.</li>
+                <li><Sub>Sec. 4.</Sub> Financial transactions of an amount less than Rs. 3000 can be carried out by the Chapter/AG ExCom after discussing the same with Chapter/Affinity Group Advisor.</li>
+            </List>
+            <Article>Article VII: Event Committees</Article>
+            <List>
+                <li><Sub>Sec. 1.</Sub> For Society Chapter and Affinity Group events, the corresponding ExCom members (Society/AG Chairperson, Vice-Chair and Secretary) along with the Student Branch Chairperson shall be the Convenors.</li>
+                <li><Sub>Sec. 2.</Sub> ExCom members shall be core committee members and only in the absence of a suitable member from ExCom, an external IEEE volunteer in the SB is to be selected after approval in an ExCom meeting with the presence of Branch Counselor.</li>
+                <li><Sub>Sec. 3.</Sub> All members of the event committee (Core and Volunteer) must be IEEE members.</li>
+                <li><Sub>Sec. 4.</Sub> Society/AG Advisor(of the corresponding Society/AG conducting the event) and Branch Counselor should be advisors for the event.</li>
             </List>
         </Innerdiv>
     )
@@ -70,7 +238,7 @@ const Innerdiv = styled.div`
 
 const Article = styled.h3`
     color #000000;
-    margin:19px 0px;
+    margin:25px 0px;
 `;
 
 const List = styled.ul`
