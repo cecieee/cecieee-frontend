6c1466bf6b6ee0c48833561d9adb54b738b040cb
noel-jose <noeljosetp@gmail.com>
2021-11-27 11:30:17 +0530

    Changed text in About ieee sb cec section

--- src/components/About.js
+++ src/components/About.js
@@ -25,7 +25,7 @@ const About = () => (
       <AboutHead>IEEE SB CEC </AboutHead>
      
       <AboutText>
-      IEEE Student Branch College of Engineering Chengannur was formed in mid-1997, with the goal of keeping the students in touch with the advancing technology. 
+      IEEE Student Branch College of Engineering Chengannur was formed in mid-1997, with the goal of keeping the students in touch with advancing technology. 
       It is equipped with an IEEE library that was inaugurated in December 1999 and currently functions as the headquarters of IEEE SB CEC.  
       The Student Branch comes under the Kochi Hub of IEEE Kerala Section and is one of the most active Student Branches in the country. 
       IEEE SB CEC has made numerous contributions to its student  community that has received widespread recognition across the globe and is the first Student Branch in the Asia-Pacific Region to establish a Women in Engineering Affinity Group.
